2d98945ac39eb66a1f3ec90821bd30b3198a4ced
Muh. Haikal Baadilah <haikalcoy137@gmail.com>
2026-07-15 13:28:49 +0800

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,14 @@
+// Animasi halaman
+window.onload=function(){
+
+document.body.style.opacity="1";
+
+};
+
+// Konfirmasi hapus
+
+function konfirmasiHapus(){
+
+return confirm("Apakah Anda yakin ingin menghapus data ini?");
+
+}
